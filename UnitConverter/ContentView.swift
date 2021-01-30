1f0e6db906a2fbad7ba879c48101e46b6da15e34
travisbrigman <travis1000@gmail.com>
2021-01-29 20:13:20 -0600

    initial commit

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var userValue = ""
+    @State private var inputUnit = 1
+    @State private var outputUnit = 1
+    
+    let unitNames: [UnitLength] = [.astronomicalUnits,.centimeters,.decameters,.decimeters,.fathoms,.furlongs,.hectometers,.kilometers,.lightyears,.megameters,.meters,.meters,.miles,.millimeters,.scandinavianMiles,.inches, .feet]
+    
+    var answer: Double {
+        let valueToConvert = Measurement(value: Double(userValue) ?? 0, unit: unitNames[inputUnit])
+        
+        return valueToConvert.converted(to: unitNames[outputUnit]).value
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        NavigationView{
+            Form{
+                Section{
+                    TextField("value", text: $userValue)
+                        .keyboardType(.decimalPad)
+                }
+                Section(header: Text("pick a source unit")){
+                    Picker("Unit", selection: $inputUnit) {
+                        
+                        ForEach(0 ..< unitNames.count) {
+                            Text("\(self.unitNames[$0].symbol)")
+                        }
+                    }
+                }
+                Section(header: Text("pick a destination unit")){
+                    Picker("Unit", selection: $outputUnit) {
+                        
+                        ForEach(0 ..< unitNames.count) {
+                            Text("\(self.unitNames[$0].symbol)")
+                        }
+                    }
+                }
+                Text("\(answer, specifier: "%.4g")")
+                
+            }
+        }
     }
 }
 
